69a208ffa4af855305fc4f432852f44e6531e53c
thant01 <thantthantoo.810@gmail.com>
2025-11-20 20:57:11 +0900

    implement flagged_count()

--- components.py
+++ components.py
@@ -173,7 +173,11 @@ class Board:
 
     def flagged_count(self) -> int:
         # TODO: Return current number of flagged cells.
-        pass
+        count = 0
+        for cell in self.cells:
+            if cell.state.is_flagged:
+                count += 1
+            return count
 
     def _reveal_all_mines(self) -> None:
         """Reveal all mines; called on game over."""
@@ -189,6 +193,7 @@ class Board:
             for cell in self.cells:
                 if not cell.state.is_revealed and not cell.state.is_mine:
                     cell.state.is_revealed = True
+
 
 
 
